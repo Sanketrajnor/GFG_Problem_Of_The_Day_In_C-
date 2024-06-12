d63cf879bcb097920f993346bab6edf0f557fd43
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2024-06-12 12:45:22 +0530

    Create ConvertArrIntoZigZagFashion.cpp

---
+++ ConvertArrIntoZigZagFashion.cpp
@@ -0,0 +1,14 @@
+void zigZag(int n, vector<int> &arr) {
+        // code here
+         for(int i=0; i<n-1; i++)
+          {
+            if(i%2 == 0 && arr[i] > arr[i+1])
+            {
+              swap(arr[i], arr[i+1]);
+            }
+            else if(i%2 != 0 && arr[i] < arr[i+1] )
+            {
+                  swap(arr[i],arr[i+1]);
+            }
+          }
+    }
