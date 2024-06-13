5e11e923bda45ace73d45050bcc755206fc6e034
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2024-06-13 08:58:34 +0530

    Create PadovanSeq.cpp

---
+++ PadovanSeq.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    long long MOD=1e9+7;
+    int padovanSequence(int n)
+    {
+       //code here
+       int arr[n+1];
+       arr[0]=1;
+       arr[1]=1;
+       arr[2]=1;
+       for(int i=3;i<=n;i++) arr[i]=(arr[i-2]+arr[i-3])%MOD;
+       return arr[n];
+    }
+    
+};
