0682ae7420570e2a6bd39aa68c910dc43995f996
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2024-06-15 10:39:00 +0530

    Create MissNumInArr.cpp

---
+++ MissNumInArr.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, vector<int>& arr) {
+        int sum= (n*(n+1))/2;
+        int s=0;
+        for(int i=0;i<n-1;i++){
+            s+=arr[i];
+        }
+        return(sum-s);
+    }
+};
