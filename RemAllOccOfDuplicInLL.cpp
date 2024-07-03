e012d202e70805c2e9117ffb48bf2a2ec40b7e60
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2024-07-03 08:56:05 +0530

    Update RemAllOccOfDuplicInLL.cpp

--- RemAllOccOfDuplicInLL.cpp
+++ RemAllOccOfDuplicInLL.cpp
@@ -1,1 +1,20 @@
-
+Node* removeAllDuplicates(struct Node* head) {
+        Node *newHead = new Node(-1); 
+        Node *currHead = newHead;  
+        Node *curr = head; 
+        while(curr) {
+            int cnt = 1; 
+            while(curr->next && curr->data == curr->next->data) {
+                cnt++;              
+                curr = curr->next; 
+            }
+            
+            if(cnt == 1) {
+                currHead->next = curr; 
+                currHead = currHead->next; 
+            }
+            curr = curr->next; 
+        }
+        currHead->next = NULL; 
+        return newHead->next;
+}
