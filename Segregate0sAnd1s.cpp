c06c959b75ee9739ee3853fd9d8be869883f0cdd
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2024-07-14 10:23:24 +0530

    Create Segregate0sAnd1s.cpp

---
+++ Segregate0sAnd1s.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+       int i =0,j=0,n =arr.size();
+        while(j < n)
+        {
+            if(arr[j] == 0)
+            {
+                swap(arr[i],arr[j]);
+                i++;
+            }
+            j++;
+        }
+    }
+};
