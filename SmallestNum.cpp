eddddbb54dad94090bb874a5f908f12d7dd9f2a8
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2024-07-15 10:01:53 +0530

    Create SmallestNum.cpp

---
+++ SmallestNum.cpp
@@ -0,0 +1,27 @@
+string smallestNumber(int s, int d) {
+        // code here
+        if (s > 9 * d) {
+            return "-1";
+        }
+        
+        string st ="";
+        for(int i=d-1;i>=0;i--){
+            
+            for(int j=9;j>=0;j--){
+                
+                if(i != 0){
+                    if(s - j > 0){
+                        st += j +'0';
+                        s = s-j;
+                        break;
+                    }
+                }
+                else{
+                    st += s +'0';
+                    break;
+                }
+            }
+        }
+        reverse(st.begin(),st.end());
+        return st;
+    }
